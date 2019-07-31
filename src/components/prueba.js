4d5345c439afd3c288b2eb4ce2ece94e283e4ad6
Andres Carranza <diacarry@gmail.com>
2019-07-30 22:56:42 -0500

    Creacion y renderizacion de un componente, clase 3

---
+++ src/components/prueba.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Prueba extends React.Component {
+    render() {
+        return <div>
+            <h1>este es un</h1>
+            <p>COMPONENTE</p>
+        </div>;
+    }
+}
+
+export default Prueba;

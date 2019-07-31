8091ee82f947e891cbba9a4f415471aee956f1ab
Andres Carranza <diacarry@gmail.com>
2019-07-30 23:35:29 -0500

    Uso de props en un componente, clase 4

---
+++ src/components/prueba.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Prueba extends React.Component {
+    render() {
+        return <div>
+            <h1>{this.props.saludo}</h1>
+            <p>COMPONENTES</p>
+        </div>;
+    }
+}
+
+export default Prueba;
